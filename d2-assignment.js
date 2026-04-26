5d8f19bcc2dc040bc6eb7e6fd67f147f888af6a6
Jagadheesan <saftron@gmail.com>
2026-04-26 07:44:33 -0500

    Add files via upload

---
+++ d2-assignment.js
@@ -0,0 +1,16 @@
+//Write a JavaScript function named `isOddOrEven` that takes an integer as input and returns `Odd` 
+// if the number is odd and `"Even"` if the number is even. 
+
+let number;
+function isOddOrEven(number) {
+    
+    if(number%2) {
+            console.log("Number you entered is ODD");
+            
+        } else {
+            console.log("Number you entered is EVEN");
+        }
+        
+    }
+
+isOddOrEven(232323232323)
